999fe34529033dd92a1be16ef8b7b183eda480b6
tuoippp <n.renassiapun0suy@hotmail.com>
2024-05-10 18:02:53 +0700

    start commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const isPrime = (num) => {
-  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
-    if (num % i === 0) return false;
-  }
-  return num > 1;
-};
+const sum = (a, b) => a + b;
