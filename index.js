8b43d084fdcba8248f1447270a348c64349a420c
tuoippp <n.renassiapun0suy@hotmail.com>
2024-05-11 23:44:46 +0700

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const sum = (a, b) => a + b;
+const pullAtIndex = (arr, pullArr) => {
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !pullArr.includes(i));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+  return removed;
+};
