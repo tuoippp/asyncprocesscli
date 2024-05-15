adbda68853844d9295019c51307911768c8930cd
tuoippp <n.renassiapun0suy@hotmail.com>
2024-05-15 12:35:13 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function isPalindrome(str) {
-  return str === str.split("").reverse().join("");
+function partitionLabels(s) {
+  const last = new Array(26);
+  for (let i = 0; i < s.length; i++) {
+    last[s.charCodeAt(i) - "a".charCodeAt(0)] = i;
+  }
+  let anchor = 0;
+  let j = 0;
+  const result = [];
+  for (let i = 0; i < s.length; i++) {
+    j = Math.max(j, last[s.charCodeAt(i) - "a".charCodeAt(0)]);
+    if (i === j) {
+      result.push(i - anchor + 1);
+      anchor = i + 1;
+    }
+  }
+  return result;
 }
